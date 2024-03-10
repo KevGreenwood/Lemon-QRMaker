ff52ebfd9d85c9c0573ac00b2a018d9109c04b86
Kevin Greenwood <kevinrecalde324@outlook.com>
2024-03-09 20:01:24 -0500

    Update commit

--- main.py
+++ main.py
@@ -1,84 +1,36 @@
-import flet as ft
 import qrcode
+import PIL
+import io
 import base64
-from io import BytesIO
-from PIL import Image
 
+class QRGenerator:
+    def __init__(self):
+        self.data = ""
+        self.version = 0
+        self.box_size = 0 # base: 29, so, just multiply by yout desire number for scale your qr 
+        self.border = 0
 
-def main(page: ft.Page):
-    page.title = "Cassie QRCode Generator"
-    page.horizontal_alignment = "center"
-    page.auto_scroll = True
+        self.back_color = ""
+        self.main_color = ""
+        self.alt_color = ""
 
-    page.update()
-    
+        self.logo_path = ""
+        self.qr_path = ""
+        self.preview = ""
 
-    def generarQr(e):
-        
-        page.update()
-        texto_error.value = ""
-        texto_ok.value = ""
-        contControles = len(page.controls)
-        if txt.value == "":
-            btn_guardar.disabled = True
-            texto_error.value = "No hay texto para convertir a QR"
-            texto_ok.value == ""
-            # si hay 6 controles, es de una generacion previa, como no se 
-            # escribio nada, elimina el ultimo control agregado que es la img del QR
-            if contControles >= 6:
-                page.controls.pop()
-            page.update()
-        else:
-            texto_error.value = ""
-            btn_guardar.disabled = False
-            url = construyeQr(txt.value)
-            img = ft.Image(src_base64=url)
-            if contControles >= 6:
-                page.controls.pop()
-            texto_ok.value = "QR generado con exito"
-            page.add(ft.Row([img]))
-            page.update()
-    
-    def construyeQr(s):
-        qr = qrcode.make(s)
-        buffered = BytesIO()
-        qr.save(buffered, format="JPEG")
-        consBuff = base64.b64encode(buffered.getvalue())
-        resultOfQrCode = consBuff.decode("utf-8")
-        return resultOfQrCode
-    
-    def guardarImagen(e):
-        url = construyeQr(txt.value)
-        nombre_archivo = "qr_guardado.png"
-        decodificarImg = base64.b64decode(url)
-        img = Image.open(BytesIO(decodificarImg))
-        img.save(nombre_archivo)
-    
+    def generate(self):
+        qr = qrcode.QRCode(
+                            version=self.version, 
+                            error_correction=qrcode.constants.ERROR_CORRECT_L,
+                            box_size=self.box_size,
+                            border=self.border
+                        )
+        qr.add_data(self.data)
+        qr.make(fit=True)
 
-    def textbox_changed(e):
-        if txt.value != "":
-            btn.disabled = False
-        else:
-            btn.disabled = True
-        page.update()
+        img = qr.make_image(fill_color=self.main_color, back_color=self.back_color)
 
-    txt = ft.TextField(label="Convertir a QR", multiline=True, on_change=textbox_changed)
-    sizeField = ft.TextField(label="Ingrese un numero entero del 1 - 40")
-    box_sizeField = ft.TextField(label="Ingrese un numero entero del 1 - 40")
-    borderField = ft.TextField(label="Ingrese un numero entero del 1 - 40")
-    errorDrop = ft.Dropdown(label="Selecciona el nivel de correcion de error", options=[ft.dropdown.Option("7%"), ft.dropdown.Option("15% (Default)"), ft.dropdown.Option("25%"), ft.dropdown.Option("30%")])
-    qr_styleDrop = ft.Dropdown(label="Seleccione el estilo que desea aplicar", options=[ft.dropdown.Option("Normal"), ft.dropdown.Option("Gapped Square"), ft.dropdown.Option("Circle"), ft.dropdown.Option("Rounded"), ft.dropdown.Option("Vertical Bars"), ft.dropdown.Option("Horizontal Bars")])
-    color_styleDrop = ft.Dropdown(label="Seleccione el estilo de color que desea aplicar", options=[ft.dropdown.Option("Normal"), ft.dropdown.Option("Radial Gradient"), ft.dropdown.Option("Square Gradient"), ft.dropdown.Option("Horizontal Gradient"), ft.dropdown.Option("Vertical Gradient"), ft.dropdown.Option("Image")])
-    embed_imageBtn = ft.ElevatedButton("Seleccione su Imagen", icon=ft.icons.IMAGE_SEARCH)
-    btn = ft.ElevatedButton("Generar QR",
-                            on_click=generarQr, disabled=True, icon=ft.icons.QR_CODE)
-    btn_guardar = ft.ElevatedButton("Guardar Imagen QR", on_click=guardarImagen, disabled=True, icon=ft.icons.SAVE_AS)
-    logoBtn = ft.ElevatedButton("Seleccione su Logo", icon=ft.icons.IMAGE_SEARCH)
-    
-    
-    texto_error = ft.Text(color="red")
-    texto_ok = ft.Text(color="green")
-
-    page.add(ft.Column([txt, ft.Row(alignment=ft.MainAxisAlignment.CENTER, controls= [sizeField, box_sizeField, borderField]), ft.Row(alignment=ft.MainAxisAlignment.CENTER, controls= [errorDrop, qr_styleDrop, color_styleDrop]), ft.Row(alignment=ft.MainAxisAlignment.CENTER, controls= [logoBtn, embed_imageBtn]), btn, texto_error, texto_ok, btn_guardar]))
-
-ft.app(target=main)
+        byte_arr = io.BytesIO()
+        img.save(byte_arr, format='JPEG')
+        encoded_image = base64.b64encode(byte_arr.getvalue()).decode("utf-8")
+        self.preview = encoded_image
--- main_design.py
+++ main_design.py
@@ -1,10 +1,12 @@
 import flet as ft
+from main import *
+
 
 def main(page: ft.Page):
     page.scroll = "always"
-    
+
     input_txt = ft.TextField(label="Ingrese el contenido", value="https://github.com/KevGreenwood")
-    type_dropdown = ft.Dropdown(options=[
+    type_dropdown = ft.Dropdown(value="General", options=[
         ft.dropdown.Option("General"),
         ft.dropdown.Option("Email"),
         ft.dropdown.Option("SMS"),
@@ -14,30 +16,28 @@ def main(page: ft.Page):
         ft.dropdown.Option("WiFi"),
         ft.dropdown.Option("Mecard")
         ])
-    
     input_col = ft.Column([type_dropdown, input_txt])
-
     input_panel = ft.ExpansionPanelList([ft.ExpansionPanel(header=ft.ListTile(title=ft.Text("ENTER CONTENT")), expanded=True, content=input_col)])
+
+    size_slider = ft.Slider(min=1, max=40, divisions=39, label="{value}")
+    size_row = ft.Row([size_slider, ft.Checkbox(label="Auto", value=True)])
+    border_txt = ft.TextField(label="Ingrese el tamaño del borde", value="4")
+    size_panel = ft.ExpansionPanelList([ft.ExpansionPanel(header=ft.ListTile(title=ft.Text("SET SIZE")), content=ft.Column([size_row, border_txt]))])
+    
 
     color_radio_group = ft.RadioGroup(content=ft.Row([
         ft.Radio(label="Single Color"),
         ft.Radio(label="Color Gradient"),
         ft.Checkbox(label="Custome Eye Color")
     ]))
-
-    fore_color_txt = ft.TextField(label="Foreground Color", value="#000000")
-    back_color_txt = ft.TextField(label="Background Color", value="#FFFFFF")
-
+    fore_color_txt = ft.TextField(label="Foreground Color", prefix_icon=ft.icons.COLOR_LENS, value="#000000")
+    back_color_txt = ft.TextField(label="Background Color", prefix_icon=ft.icons.COLOR_LENS, value="#FFFFFF")
     fore_color_row = ft.Row([fore_color_txt])
-
     color_column = ft.Column([color_radio_group, fore_color_row, back_color_txt])
-
-
     color_panel = ft.ExpansionPanelList([ft.ExpansionPanel(header=ft.ListTile(title=ft.Text("SET COLORS")), content=color_column)])
 
     logo_preview = ft.Container(content=ft.Image(src="default-preview-qr.svg", width=200, height=200))
-    logo_column = ft.Column([logo_preview, ft.ElevatedButton("Upload Logo")])
-
+    logo_column = ft.Column([logo_preview, ft.ElevatedButton("Upload Logo", icon=ft.icons.UPLOAD_FILE_ROUNDED)])
     logo_panel = ft.ExpansionPanelList([ft.ExpansionPanel(header=ft.ListTile(title=ft.Text("ADD LOGO IMAGE")), content=logo_column)])
 
     body_shape = ft.Row([
@@ -48,7 +48,6 @@ def main(page: ft.Page):
         ft.Image(src="default-preview-qr.svg", width=100, height=100),
         ft.Image(src="default-preview-qr.svg", width=100, height=100)
         ])
-
     eye_shape = ft.Row([
         ft.Image(src="default-preview-qr.svg", width=100, height=100),
         ft.Image(src="default-preview-qr.svg", width=100, height=100),
@@ -57,7 +56,6 @@ def main(page: ft.Page):
         ft.Image(src="default-preview-qr.svg", width=100, height=100),
         ft.Image(src="default-preview-qr.svg", width=100, height=100)
         ])
-
     eye_ball = ft.Row([
         ft.Image(src="default-preview-qr.svg", width=100, height=100),
         ft.Image(src="default-preview-qr.svg", width=100, height=100),
@@ -66,22 +64,51 @@ def main(page: ft.Page):
         ft.Image(src="default-preview-qr.svg", width=100, height=100),
         ft.Image(src="default-preview-qr.svg", width=100, height=100)
         ])
-    
     styles_col = ft.Column([ft.Text("Body Shape"), body_shape, ft.Text("Eye Frame Shape"), eye_shape, ft.Text("Eye Ball Shape"), eye_ball])
-
     design_panel = ft.ExpansionPanelList([ft.ExpansionPanel(header=ft.ListTile(title=ft.Text("CUSTOMIZE DESIGN")), content=styles_col)])
+    
+    error_dropdown = ft.Dropdown(value="ERROR_CORRECT_M", options=[
+        ft.dropdown.Option("ERROR_CORRECT_L"),
+        ft.dropdown.Option("ERROR_CORRECT_M"),
+        ft.dropdown.Option("ERROR_CORRECT_Q"),
+        ft.dropdown.Option("ERROR_CORRECT_H"),
+        ])
+    error_panel = ft.ExpansionPanelList([ft.ExpansionPanel(header=ft.ListTile(title=ft.Text("ERROR CORRECTION")), content=error_dropdown)])
+    left_column = ft.Column([input_panel, size_panel, color_panel, logo_panel, design_panel, error_panel])
 
-    left_column = ft.Column(controls=[input_panel, color_panel, logo_panel, design_panel])
+    qr_preview = ft.Image(src="default-preview-qr.svg", width=300, height=300)
+    conainer = ft.Container(alignment=ft.alignment.top_center, content=qr_preview)
 
+    def build_qr():
+        qr = QRGenerator()
+        qr.data = input_txt.value
+        qr.version = size_slider.value
+        qr.box_size = box_size_slider.value
+        qr.border = int(border_txt.value)
+        qr.back_color = back_color_txt.value
+        qr.main_color = fore_color_txt.value
+        qr.generate()
+        return qr.preview
 
-    qr_preview = ft.Container(alignment=ft.alignment.top_center, content=ft.Image(src="default-preview-qr.svg", width=300, height=300))
-    size_slider = ft.Slider(min=1, max=40, divisions=40)
-    size_txt = ft.Row(alignment=ft.MainAxisAlignment.CENTER, controls=[ft.Text("Low Quality"), ft.Text("Size"),ft.Text("High Quality")])
-    buttons_row = ft.Row(alignment=ft.MainAxisAlignment.CENTER, controls=[ft.ElevatedButton("Generate"), ft.ElevatedButton("Save")])
-    right_column = ft.Column(alignment=ft.alignment.top_center, controls=[qr_preview, size_slider, size_txt, buttons_row])
+    def button_clicked(e):
+        qr_preview.src_base64 = build_qr()
+        page.update()
 
-    left = ft.Container(alignment=ft.alignment.top_left, bgcolor="#e8eef2", width=500, height=1000, col={"sm": 12, "md": 8, "xl": 8}, content=left_column)
-    right = ft.Container(bgcolor="white", width=300, height=1000, col={"sm": 12, "md": 4, "xl": 4}, content=right_column)
+    scale_txt = ft.Text("1450 x 1450 px", color="black", weight=ft.FontWeight.BOLD, text_align=ft.TextAlign.CENTER)
+
+    def update_scale_txt(e):
+        slider_value = int(e.control.value)
+        scale_txt.value = f"{29*slider_value} x {29*slider_value} px"
+        page.update()
+
+    box_size_slider = ft.Slider(min=10, max=100, divisions=9, label="{value}", value=50, on_change=update_scale_txt)
+
+    scale_column = ft.Column(alignment=ft.MainAxisAlignment.CENTER, controls=[box_size_slider, scale_txt])
+    buttons_row = ft.Row(alignment=ft.MainAxisAlignment.CENTER, controls=[ft.ElevatedButton("Generate", on_click=button_clicked, icon=ft.icons.QR_CODE_ROUNDED), ft.ElevatedButton("Save", icon=ft.icons.SAVE_ALT_ROUNDED)])
+    right_column = ft.Column(alignment=ft.alignment.top_center, controls=[conainer, scale_column, buttons_row])
+
+    left = ft.Container(alignment=ft.alignment.top_left, bgcolor="#e8eef2", width=500, height=2000, col={"sm": 12, "md": 8, "xl": 8}, content=left_column)
+    right = ft.Container(bgcolor="white", width=300, height=2000, col={"sm": 12, "md": 4, "xl": 4}, content=right_column)
     main_container = ft.ResponsiveRow(spacing=0, controls=[left, right])
 
     page.add(main_container)
