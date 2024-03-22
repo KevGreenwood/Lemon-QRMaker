77dcbe8443fb4ce6920f041b85283467143bf2b0
Kevin Greenwood <kevinrecalde324@outlook.com>
2024-03-22 11:20:29 -0500

    Update commit

--- main_designer.py
+++ main_designer.py
@@ -2,38 +2,28 @@ from flet import *
 import flet as ft
 from core import *
 
-class CustomTab(UserControl):
-    def __init__(self, icon, text: str):
-        super().__init__()
-        self.icon = icon
-        self.text = text
-
-    def build(self):
-        return Tab(tab_content=Column([Icon(self.icon), Text(self.text)], spacing=0))
-
 class QR_Tabs(UserControl):
     def __init__(self):
         super().__init__()
+        
         self.main_txt = TextField(label="Ingrese el contenido") # Use for generic porpouses
         self.alt_txt = TextField(label="Ingrese el contenido")
         self.mail_txt = TextField(label="Ingrese su correo electronico")
-
-        self.url_tab = CustomTab(icons.LINK, "URL")
-
         self.phone_txt = TextField(label="Ingrese el contenido", input_filter=NumbersOnlyInputFilter())
-        #self.url_tab = Tab(tab_content=Column([Icon(icons.LINK), Text("URL")], spacing=0))
-        self.text_tab = Tab(text="Text")
-        self.mail_tab = Tab(text="Email")
-        self.phone_tab = Tab(text="Phone")
-        self.sms_tab = Tab(text="SMS")
-        self.wa_tab = Tab(text="Whatsapp")
-        self.vcard_tab = Tab(text="VCard")
-        self.mcard_tab = Tab(text="MeCard")
-        self.location_tab = Tab(text="Location")
-        self.wifi_tab = Tab(text="WiFi")
-        self.event_tab = Tab(text="Event")
-        self.paypal_tab = Tab(text="PayPal")
-        self.bitcoin_tab = Tab(text="Bitcoin")
+        
+        self.url_tab = Tab(tab_content=Column([Icon(icons.LINK), Text("URL")], spacing=0, horizontal_alignment=CrossAxisAlignment.CENTER))
+        self.text_tab = Tab(tab_content=Column([Icon(icons.TEXT_SNIPPET), Text("Text")], spacing=0, horizontal_alignment=CrossAxisAlignment.CENTER))
+        self.mail_tab = Tab(tab_content=Column([Icon(icons.MAIL), Text("Email")], spacing=0, horizontal_alignment=CrossAxisAlignment.CENTER))
+        self.phone_tab = Tab(tab_content=Column([Icon(icons.PHONE), Text("Phone")], spacing=0, horizontal_alignment=CrossAxisAlignment.CENTER))
+        self.sms_tab = Tab(tab_content=Column([Icon(icons.SMS), Text("SMS")], spacing=0, horizontal_alignment=CrossAxisAlignment.CENTER))
+        self.wa_tab = Tab(tab_content=Column([ft.Image(src="Assets\\Icons\\whatsapp.svg", width=24, height=24, color=colors.WHITE), Text("Whatsapp")], spacing=0, horizontal_alignment=CrossAxisAlignment.CENTER))
+        self.vcard_tab = Tab(tab_content=Column([Icon(icons.CONTACT_PAGE), Text("VCard")], spacing=0, horizontal_alignment=CrossAxisAlignment.CENTER))
+        self.mcard_tab = Tab(tab_content=Column([Icon(icons.CONTACT_PAGE), Text("MeCard")], spacing=0, horizontal_alignment=CrossAxisAlignment.CENTER))
+        self.location_tab = Tab(tab_content=Column([Icon(icons.LOCATION_ON), Text("Location")], spacing=0, horizontal_alignment=CrossAxisAlignment.CENTER))
+        self.wifi_tab = Tab(tab_content=Column([Icon(icons.WIFI), Text("WiFi")], spacing=0, horizontal_alignment=CrossAxisAlignment.CENTER))
+        self.event_tab = Tab(tab_content=Column([Icon(icons.EVENT), Text("Event")], spacing=0, horizontal_alignment=CrossAxisAlignment.CENTER))
+        self.paypal_tab = Tab(tab_content=Column([Icon(icons.PAYPAL), Text("PayPal")], spacing=0, horizontal_alignment=CrossAxisAlignment.CENTER))
+        self.bitcoin_tab = Tab(tab_content=Column([Icon(icons.CURRENCY_BITCOIN), Text("Bitcoin")], spacing=0, horizontal_alignment=CrossAxisAlignment.CENTER))
 
         self.tabs = Tabs(tabs=
             [
@@ -44,39 +34,57 @@ class QR_Tabs(UserControl):
             ], on_change=self.update, selected_index=0)
         
         self.cont = Container(padding=10)
+
+        self.back = IconButton(icon=icons.ARROW_BACK_IOS, on_click=self.go_back)
+
+        self.forward = IconButton(icon=icons.ARROW_FORWARD_IOS, on_click=self.go_forward)
+
+        self.tab_container = Container(Row([self.back, self.tabs, self.forward]))
+
+    def debug(self, e):
+        print(self.tabs.selected_index)
+    
+    def go_back(self, e):
+        if self.tabs.selected_index > 0:
+            self.tabs.selected_index -= 1
+            self.update(e)
         
+    def go_forward(self, e):
+        if self.tabs.selected_index < 12:
+            self.tabs.selected_index += 1
+            self.update(e)
+    
     def update(self, e):
-        title = self.tabs.tabs[self.tabs.selected_index].text
-        if title == "URL":
+        if self.tabs.selected_index == 0:
             self.main_txt.value = "https://github.com/KevGreenwood"
             self.main_txt.multiline = False
             self.main_txt.filled = False
             self.cont.content = self.main_txt
         
-        if title == "Text":
+        if self.tabs.selected_index == 1:
             self.main_txt.value = ""
             self.main_txt.multiline = True
             self.main_txt.filled = True
             self.cont.content = self.main_txt
         
-        if title == "Email":
+        if self.tabs.selected_index == 2:
             self.mail_txt.value = ""
             self.alt_txt.value = ""
             self.main_txt.value = ""
             self.main_txt.filled = True
             self.cont.content = Column([self.mail_txt, self.alt_txt, self.main_txt])
 
-        if title == "Phone":
+        if self.tabs.selected_index == 3:
             self.phone_txt.value = ""
             self.cont.content = self.phone_txt
 
-        if title == "SMS" or title == "Whatsapp":
+        if self.tabs.selected_index == 4 or self.tabs.selected_index == 5:
             self.phone_txt.value = ""
             self.main_txt.value = ""
             self.main_txt.filled = True
             self.cont.content = Column([self.phone_txt, self.main_txt])
 
-        if title == "WiFi":
+        if self.tabs.selected_index == 6:
                 
             self.cont.content = Column([self.main_txt, self.alt_txt, ])
             
@@ -84,4 +92,4 @@ class QR_Tabs(UserControl):
         super().update()
 
     def build(self):
-        return Column([self.tabs, self.cont])
+        return Column([self.tab_container, self.cont])
